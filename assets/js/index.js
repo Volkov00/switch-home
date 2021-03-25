af920472514d47e980d0fbef9cb06d2108d83054
Volkov <twen1200712@gmail.com>
2021-03-25 12:35:05 +0200

    second

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,3 +41,102 @@ switch (day) {
     alert('Такого числа нету')
 }
 
+
+
+
+// let a = prompt('Yor number?')
+// function mounth(a) {
+//     if (isNaN(a)) return alert('WHAT??');
+//     if (a >= 0 && a <= 10) return alert('first dekada');
+//     if (a >= 10 && a <= 20) return alert('second dekada');
+//     if (a >= 20 && a <= 31) return alert('Third dekada');
+//     return alert ('Nothing');
+// }
+// alert (mounth(a));
+
+
+
+
+
+
+
+// let q = +prompt('Number1');
+// let qq = +prompt('Number2');
+// let qqq = +prompt('Введите действия \n 1.деление \n 2.умножение \n 3.сложение \n 4.вычетание');
+
+// switch (qqq) {
+//     case 1:
+//         let sum = (q / qq);
+//         alert(sum);
+//         break;
+// }
+// switch (qqq) {
+//     case 2: 
+//         alert(q * qq +' '+'Результат Вашего умножения');
+//         break;
+// }
+// switch (qqq) {
+//     case 3: 
+//         alert(q + qq +' '+'Результат Вашего сложения');
+//         break;
+// }
+// switch (qqq) {
+//     case 4: 
+//         alert(q - qq +' '+'Результат Вашего вычитания');
+//         break;
+//     default: alert('Error');
+//         break;
+// }
+
+
+
+// let q = +prompt('Number1');
+// let qq = +prompt('Number2');
+// let qqq = +prompt('Введите действия \n 1.деление \n 2.умножение \n 3.сложение \n 4.вычетание');
+// if (isNaN(q - qq - qqq)) alert('please only number');
+// if (qqq === 1) {
+//     alert(q / qq);
+// }
+// if  (qqq === 2){
+//     alert(q * qq);
+// }
+// if (qqq === 3) {
+//     alert(q + qq);
+// }
+// if (qqq === 4) {
+//     alert(q - qq);
+// }
+// else (
+//     alert('Error')
+// )
+
+
+
+
+// let v1 = +prompt('Number1');
+// let v2 = +prompt('Number2');
+// let actions = +prompt('Введите действия \n 1.+ \n 2.- \n 3.* \n 4./');
+
+// function t(v1, v2, action) {
+//     if (isNaN(v1 - v2 - actions)) {
+//         return alert ('Only Numbers Pls');
+//     }
+//     if (action === 1) {
+//         sum = v1 + v2;
+//         return alert(sum);
+//     } if  (actions === 2) {
+//         min = v1 - v2;
+//         return alert(min);
+//     } if (actions === 3) {
+//         multiplication = v1 * v2;
+//         return alert(multiplication);
+//     } if (actions === 4) {
+//         division = v1 / v2;
+//         return alert(division);
+//     } if (actions > 4) {
+//         return alert('Nothing');
+//     } else {
+//         return alert('ERROR');
+//     }
+// }
+// alert(t(v1, v2));
